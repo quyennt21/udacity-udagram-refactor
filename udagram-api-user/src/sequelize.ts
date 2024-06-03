8e32347a767b01782dc13debeb09a56318029961
Baron Nguyen <ken.tb997@gmail.com>
2024-06-03 11:56:50 +0700

    update api service

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,13 +1,15 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
-
-export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  'storage': ':memory:',
-});
+export const sequelize = new Sequelize(
+  `postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`,
+  {
+    storage: ":memory:",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
